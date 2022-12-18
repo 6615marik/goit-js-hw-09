a968d183d3b80c5b713c2f07077353eb5e0e8c95
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2022-12-18 13:10:17 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,8 +2,6 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 
-// Form and its inputs
-
 const form = document.querySelector('.form');
 
 const firstDelayMs = document.querySelector('[name="delay"]');
